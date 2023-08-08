a3247c09cdfaaf080a34c7631375d7e9cdbb4775
Alberto <albermr99@gmail.com>
2023-08-08 11:54:19 +0200

    Se agrero nueva funcionalidad cin

--- prueba.cpp
+++ prueba.cpp
@@ -4,11 +4,20 @@ using namespace std;
 
 int main()
 {
+    int entero1, entero2, entero3;
     cout<<"Hola mundo para prueba3 de git"<<endl;
     cout<<"Prueba4";
     cout<<"Prueba5";
     cout<<"Prueba Alberto";
     cout<<"Modificación Miguel";
-    cout<<"Modificación sin error";
+    cout<<"Modificación sin error"<<endl;
+    cout<<"Introduzca datos para realizar una cuenta: "
+    cin >> entero1;
+    cin >> entero2;
+    
+    entero3= entero1 + entero2;
+
+    cout << "La suma es: "<<entero3<< endl;
+
     return 0;
 }
